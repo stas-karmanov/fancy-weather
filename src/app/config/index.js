856f4df11a792d9aa472235af745c1380b5a2780
Stanislav Karmanov <stas.karmanov111@gmail.com>
2020-05-11 21:06:58 +0300

    add images service

--- src/app/config/index.js
+++ src/app/config/index.js
@@ -1,4 +1,5 @@
 export const config = {
     GEOLOCATION_API_KEY,
     WEATHER_API_KEY,
+    IMAGES_API_KEY,
 };
---
+++ src/app/services/images.js
@@ -0,0 +1,17 @@
+import { config } from '../config';
+
+class ImagesService {
+    constructor() {
+        this.endpoint = 'https://api.unsplash.com/photos';
+    }
+
+    getRandomImage() {
+        return fetch(
+            `${this.endpoint}/random?orientation=landscape&per_page=1&query=nature&client_id=${config.IMAGES_API_KEY}`,
+        )
+            .then(res => res.json())
+            .then(({ urls: { regular } }) => regular);
+    }
+}
+
+export const imagesService = new ImagesService();
