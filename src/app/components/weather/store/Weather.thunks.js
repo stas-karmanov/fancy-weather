14f022dee0b1419871c3accf3ed92fd2a38058b7
Stanislav Karmanov <stas.karmanov111@gmail.com>
2020-05-15 21:58:11 +0300

    reset errors on location change

--- src/app/components/weather/store/Weather.thunks.js
+++ src/app/components/weather/store/Weather.thunks.js
@@ -1,9 +1,11 @@
 import { weatherService } from '../../../services';
 import { setWeather } from './Weather.actions';
-import { addError } from '../../errors/store/Errors.actions';
+import { addError, resetErrors } from '../../errors/store/Errors.actions';
 import { loadCoordinatesInfo } from '../../coordinates-info/store/CoordinatesInfo.thunks';
 
 export const loadWeatherInfo = (city, locale, abortController) => dispatch => {
+    dispatch(resetErrors());
+
     weatherService
         .getWeatherInfo({ region: city, forecastLength: 4, abortController, locale })
         .then(weatherInfo => {
